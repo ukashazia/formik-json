83818f85b11ad019900ac2bd78ac4d497eebcfaf
Ukasha Zia <ukashazia41@gmail.com>
2023-12-01 15:30:25 +0500

    Update CodeEditor.js

--- src/Field/CodeEditor.js
+++ src/Field/CodeEditor.js
@@ -1,42 +1,46 @@
-import React from 'react';
-import {Controlled as CodeMirror} from 'react-codemirror2';
-import {changeHandler, setFieldValueWrapper} from '../utils';
+import React from "react";
+import { changeHandler, setFieldValueWrapper } from "../utils";
+import CodeMirror from "@uiw/react-codemirror";
+import { loadLanguage } from "@uiw/codemirror-extensions-langs";
+import { githubLight } from "@uiw/codemirror-theme-github";
 
-const CodeEditor = ({config, formik, value, error}) => {
-    const {
-        name,
-        options,
-        defaultValue,
-        attributes,
-        fieldClass = ''
-    } = config;
-    const {setFieldValue, handleBlur} = formik;
-    const selectedValue = value || '';
+const CodeEditor = ({ config, formik, value, error }) => {
+  const {
+    name,
+    language,
+    options,
+    attributes,
+    fieldClass = "",
+  } = config;
 
-    return (
-        <CodeMirror
-            id={name}
-            name={name}
-            options={options}
-            className={fieldClass + (error ? ' is-invalid ' : '')}
-            onBeforeChange={(editor, data, value) => {
-                changeHandler(setFieldValueWrapper(setFieldValue, name), formik, config, value)
-            }}
-            onBlur={(editor, event) => {
-                return (
-                    handleBlur({
-                        ...event,
-                        target: {
-                            ...event.target,
-                            name
-                        }
-                    })
-                );
-            }}
-            value={selectedValue}
-            {...attributes}
-        />
-    );
-}
+  const { setFieldValue, handleBlur } = formik;
+  const selectedValue = value || "";
+
+  return (
+    <div className={"position-relative"}>
+      <div className="position-absolute bg-white shadow px-2 py-0 rounded-2 top-0 end-0 z-1">
+        {language}
+      </div>
+      <CodeMirror
+        value={selectedValue}
+        extensions={[loadLanguage(language)]}
+        theme={githubLight}
+        id={name}
+        name={name}
+        className={fieldClass + (error ? " is-invalid " : "")}
+        onChange={(value, viewUpdate) => {
+          changeHandler(
+            setFieldValueWrapper(setFieldValue, name),
+            formik,
+            config,
+            value,
+          );
+        }}
+        {...options}
+        {...attributes}
+      />
+    </div>
+  );
+};
 
 export default React.memo(CodeEditor);
